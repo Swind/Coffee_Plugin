5515d99fd5db33eac80e6a28ae6579e894a1e03a
Swind <swind@code-life.info>
2015-02-17 16:11:02 +0000

    Add datetime format

--- static/js/coffee.js
+++ static/js/coffee.js
@@ -1,5 +1,30 @@
-var cookbook_list;
+var cookbook_list, Cookbooks, Cookbook;
 cookbook_list = {};
+Cookbooks = function(raw_data){
+  var name, metadata, results$ = [];
+  for (name in raw_data) {
+    metadata = raw_data[name];
+    results$.push(new Cookbook(name, metadata));
+  }
+  return results$;
+};
+Cookbook = (function(){
+  Cookbook.displayName = 'Cookbook';
+  var prototype = Cookbook.prototype, constructor = Cookbook;
+  prototype.format_date = function(date_time){
+    var dd;
+    dd = new Date(date_time * 1000);
+    return dd.getFullYear() + '/' + (dd.getMonth() + 1) + "/" + dd.getDate() + " " + dd.getHours() + ":" + dd.getMinutes() + ":" + dd.getSeconds();
+  };
+  function Cookbook(name, metadata){
+    this.name = name;
+    this.created_date = this.format_date(metadata['date']);
+    this.estimated_time = metadata["analysis"]["estimatedPrintTime"];
+    this.volume = metadata["analysis"]["filament"]["tool0"]["volume"];
+    this.length = metadata["analysis"]["filament"]["tool0"]["length"];
+  }
+  return Cookbook;
+}());
 cookbook_list.vm = function(){
   var vm;
   vm = {};
@@ -8,20 +33,21 @@ cookbook_list.vm = function(){
   };
   vm.cookbooks = m.request({
     method: 'GET',
-    url: '/plugin/coffee/cookbooks'
+    url: '/plugin/coffee/cookbooks',
+    type: Cookbooks
   });
   return vm;
 }();
 cookbook_list.view = function(ctrl){
-  var generate_item, name, metadata;
-  generate_item = function(name){
-    return m("div.ui.item", [m("div.content", [m("a.header", name)])]);
+  var generate_item, cookbook;
+  generate_item = function(cookbook){
+    return m("div.ui.item", [m("div.content", [m("a.header", cookbook.name), m("div.meta", cookbook.created_date), m("div.description", [m("div", cookbook.estimated_time), m("div", cookbook.volume), m("div", cookbook.length)])])]);
   };
-  return m("div", (function(){
-    var ref$, results$ = [];
-    for (name in ref$ = cookbook_list.vm.cookbooks()) {
-      metadata = ref$[name];
-      results$.push(generate_item(name));
+  return m("div.ui.items", (function(){
+    var i$, ref$, len$, results$ = [];
+    for (i$ = 0, len$ = (ref$ = cookbook_list.vm.cookbooks()).length; i$ < len$; ++i$) {
+      cookbook = ref$[i$];
+      results$.push(generate_item(cookbook));
     }
     return results$;
   }()));
--- static/ls/coffee.ls
+++ static/ls/coffee.ls
@@ -1,27 +1,50 @@
 cookbook_list = {}
 
+Cookbooks = (raw_data) ->
+    for name, metadata of raw_data
+        new Cookbook(name, metadata)
+
+class Cookbook 
+    format_date: (date_time) ->
+        dd = new Date(date_time * 1000)
+        dd.getFullYear! + '/' + (dd.getMonth! + 1) + "/" + dd.getDate! + " " +
+            dd.getHours! + ":" + dd.getMinutes! + ":" + dd.getSeconds!
+
+    (name, metadata) ->
+        @name = name
+        @created_date = @format_date(metadata['date'])
+        @estimated_time = metadata["analysis"]["estimatedPrintTime"]
+        @volume = metadata["analysis"]["filament"]["tool0"]["volume"]
+        @length = metadata["analysis"]["filament"]["tool0"]["length"]
+
 cookbook_list.vm = do ->
     vm = {}
 
     vm.init = ! ->
         vm.list = []
 
-    vm.cookbooks = m.request {method: 'GET', url: '/plugin/coffee/cookbooks'}
+    vm.cookbooks = m.request {method: 'GET', url: '/plugin/coffee/cookbooks', type: Cookbooks}
+
     vm
 
 cookbook_list.view = (ctrl) ->
-
-    generate_item = (name) ->
+    generate_item = (cookbook) ->
         m "div.ui.item", [
-            m "div.content", [
-                m "a.header" name
-            ]
+            (m "div.content", [
+                (m "a.header" cookbook.name),
+                (m "div.meta" cookbook.created_date),
+                (m "div.description", [
+                    (m "div" cookbook.estimated_time),
+                    (m "div" cookbook.volume),
+                    (m "div" cookbook.length)
+                ])
+            ])
         ]
 
-    m "div", for name, metadata of cookbook_list.vm.cookbooks!
-        generate_item(name)
+    m "div.ui.items", for cookbook in cookbook_list.vm.cookbooks!
+        generate_item(cookbook)
 
 cookbook_list.controller = ! ->
     cookbook_list.vm.init!
 
-m.module (document.getElementById "sidebar"), cookbook_list 
+m.module (document.getElementById "sidebar"), cookbook_list
